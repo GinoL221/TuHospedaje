eaf85f136af9154d96b019e0bd6bd245b52ab80c
Gino <ginolencina@gmail.com>
2026-09-20 23:41:12 -0300

    style: format favorites and category pages

--- frontend/src/pages/Home/CategoryCard.jsx
+++ frontend/src/pages/Home/CategoryCard.jsx
@@ -1,20 +1,20 @@
 import Icon from "../../components/Icons/Icon";
 
 export default function CategoryCard({ category, isActive, onClick }) {
-  return (
-    <button
-      type="button"
-      className={"category-tag" + (isActive ? " active" : "")}
-      onClick={onClick}
-      aria-pressed={isActive}
-    >
-      <span className="category-icon" aria-hidden="true">
-        <Icon name={category.icon} size={24} />
-      </span>
-      <span className="category-name">{category.name}</span>
-      {category.description && (
-        <span className="category-description">{category.description}</span>
-      )}
-    </button>
-  );
+	return (
+		<button
+			type="button"
+			className={"category-tag" + (isActive ? " active" : "")}
+			onClick={onClick}
+			aria-pressed={isActive}
+		>
+			<span className="category-icon" aria-hidden="true">
+				<Icon name={category.icon} size={24} />
+			</span>
+			<span className="category-name">{category.name}</span>
+			{category.description && (
+				<span className="category-description">{category.description}</span>
+			)}
+		</button>
+	);
 }
